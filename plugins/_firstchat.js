b887855c28de7d1271653eeb69922b85c0f3bb5c
Adhwa99811 <98006555+Adhwa99811@users.noreply.github.com>
2022-02-28 15:04:01 +0800

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -8,8 +8,8 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${this.ucapan()}
 
-${user.banned ? 'kamu dibanned' : `Ada yang bisa ${this.user.name} bantu?`}
-`.trim(), '© stikerin', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+${user.banned ? 'kamu dibanned' : `Click the button below to get how to make a sticker using whatsapp bot.`}
+`.trim(), '©2022 by Wan', user.banned ? 'Pemilik Bot' : 'Tutorial', user.banned ? ',owner' : '.t', m)
     user.pc = new Date * 1
 }
 
